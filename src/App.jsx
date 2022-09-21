76b7ebfeaaf926e64233ed54b5229c014488e0f7
IngNex <ingnexmaicol14@gmail.com>
2022-09-21 10:58:53 -0500

    add theme project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
+import { Helmet } from 'react-helmet';
 import { ThemeProvider } from 'styled-components';
+import { GlobalStyled } from './styles/GlobalStyle';
+import { darkTheme, lightTheme } from './styles/theme';
 
 export const ThemeContext = React.createContext(null);
 
@@ -9,9 +12,17 @@ function App() {
   const themeStyle = theme === 'light' ? lightTheme : darkTheme;
 
   return (
-    <ThemeProvider theme={theme}>
-      Hola
-    </ThemeProvider>
+    <ThemeContext.Provider value={{ setTheme, theme }}>
+      <ThemeProvider theme={themeStyle}>
+        <GlobalStyled/>
+        <Helmet>
+          <title>Sidebar - DarkLight</title>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=Clicker+Script&family=Poppins:wght@400;500&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+        </Helmet>
+      </ThemeProvider>
+    </ThemeContext.Provider>
   )
 }
 
---
+++ src/styles/variables.jsx
@@ -0,0 +1,15 @@
+import {css} from "styled-components";
+
+export const v = {
+    sidebarWidth: `300px`,
+    smSpacing: `8px`,
+    mdSpacing: `16px`,
+    lgSpacing: `24px`,
+    xlSpacing: `32px`,
+    xxlSpacing: `48px`,
+    borderRadius: `6px`,
+}
+
+export const btnReset = css`
+    font-family: inherit;
+`
